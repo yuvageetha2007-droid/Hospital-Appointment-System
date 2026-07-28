e57f3a148fe7a39a619ae1a41e51450d34f8ecc4
yuvageetha2007-droid <yuvageetha2007@gmail.com>
2026-07-28 17:05:02 +0530

    Create style css .java

---
+++ style css .java
@@ -0,0 +1,33 @@
+body{
+    font-family: Arial;
+    background:#f2f7ff;
+    text-align:center;
+}
+
+header{
+    background:#2c7be5;
+    color:white;
+    padding:20px;
+}
+
+.container{
+    margin:50px auto;
+    width:350px;
+    background:white;
+    padding:25px;
+    border-radius:10px;
+}
+
+input{
+    width:90%;
+    padding:10px;
+    margin:10px;
+}
+
+button{
+    padding:10px 25px;
+    background:#2c7be5;
+    color:white;
+    border:none;
+    border-radius:5px;
+}
